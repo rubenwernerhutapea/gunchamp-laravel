62d5a6cc2b4781c25742d8705b781f526bc70301
Aska Azzahra <aska.azzahra141100@gmail.com>
2022-07-23 22:37:44 +0700

    finish

--- resources/views/includes/footer-details.blade.php
+++ resources/views/includes/footer-details.blade.php
@@ -6,21 +6,21 @@
             <a href="{{ route('home') }}" class="logo"><img src="{{ url('/frontend/img/logo-white.png') }}" height="50" /></a>
             <p class="pt-3">Kami akan selalu memberikan yang terbaik untuk para champions Universitas Gunadarma.</p>
             <div class="sosmed">
-              <a href="#"><i class="bi bi-facebook px-3 text-white"></i></a>
-              <a href="#"><i class="bi bi-instagram px-3 text-white"></i></a>
-              <a href="#"><i class="bi bi-linkedin px-3 text-white"></i></a>
-              <a href="#"><i class="bi bi-twitter px-3 text-white"></i></a>
+              <a href="https://www.facebook.com/gunadarma/" target="_blank"><i class="bi bi-facebook px-3 text-white"></i></a>
+              <a href="https://www.instagram.com/gunadarma/" target="_blank"><i class="bi bi-instagram px-3 text-white"></i></a>
+              <a href="https://www.linkedin.com/school/gunadarmauniversity/" target="_blank"><i class="bi bi-linkedin px-3 text-white"></i></a>
+              <a href="https://twitter.com/gunadarma_" target="_blank"><i class="bi bi-twitter px-3 text-white"></i></a>
             </div>
           </div>
 
           <div class="col-md-2 mt-3">
             <p class="font-weight-bold">Tentang</p>
-            <a href="#" class="pt-3 text-white">Aska Azzahra</a>
+            <a href="https://www.linkedin.com/in/aska-azzahra/" target="_blank" class="pt-3 text-white">Aska Azzahra</a>
           </div>
 
           <div class="col-md-2 mt-3">
             <p class="font-weight-bold">Support</p>
-            <a href="#" class="pt-3 text-white">Gunadarma University</a>
+            <a href="https://www.gunadarma.ac.id/" target="_blank" class="pt-3 text-white">Gunadarma University</a>
             <br />
             <a href="#" class="pt-3 text-white">Kelas 3KA01</a>
           </div>
